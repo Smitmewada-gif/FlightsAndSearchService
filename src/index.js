b2807493888040abae0df21c55555d71467b3425
SmitMewada-gif <mewadasmit72@gmail.com>
2025-05-02 14:27:33 +0530

    Added router to completer create city api and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
+const Apiroutes = require('./routes/index');
 
 const setUpAndStartSever = async () => {
 
@@ -9,6 +10,8 @@ const setUpAndStartSever = async () => {
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
+  
+  app.use('/api', Apiroutes);
 
   app.listen(PORT, ()=>{
     console.log(`Server started at port ${PORT}`);
